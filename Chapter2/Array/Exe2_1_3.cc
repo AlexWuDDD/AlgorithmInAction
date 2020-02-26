00480a429924de2d4a931c536400c8b85d5963a3
AlexWuDDD <wkjworking@outlook.com>
2020-02-27 05:42:19 +0800

    fix the layout

--- Chapter2/Array/Exe2_1_3.cc
+++ Chapter2/Array/Exe2_1_3.cc
@@ -1,51 +1,6 @@
-#ifndef EXE2_1_3_H
-#define EXE2_1_3_H
+#include "Exe2_1_3.h"
 
-#include <vector>
-using std::vector;
-
-/*
-2.1.3 Search in Rotated Sorted Array
-suppose a sorted array is rotated at some pivot unknown to your beforehand
-(i.e. 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2)
-You are given a target value to search. If found in the array return its index, otherwise return -1
-You may assume to duplicate exists in the array
-*/
-
-class Solution
+int main()
 {
-public:
-    int search(const vector<int> &nums, int target)
-    {
-        int first = 0, last = nums.size();
-        while(first != last){
-            const int mid = first + (last - first) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            if(nums[first] <= nums[mid]){
-                if(nums[first] <=target && target < nums[mid]){
-                    last = mid;
-                }
-                else{
-                    first = mid;
-                }
-            }
-            else{
-                if(nums[mid] < target && target <= nums[last-1]){
-                    first = mid + 1;
-                }
-                else{
-                    last = mid;
-                }
-            }
-        }
-        return -1;
-    }
-};
-
-
-
-
-
-#endif
+    return 0;
+}
